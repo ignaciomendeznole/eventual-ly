61087864e1fb9c4734d8a90dc24297e71db69736
Ignacio Mendez <>
2021-08-18 11:53:11 -0300

    -

--- src/screens/NewEvent/styles.tsx
+++ src/screens/NewEvent/styles.tsx
@@ -5,7 +5,7 @@ import fonts from '../../constants/fonts';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 70,
+    paddingTop: Platform.OS === 'ios' ? 70 : 20,
   },
   titleContainer: {
     paddingHorizontal: 15,
